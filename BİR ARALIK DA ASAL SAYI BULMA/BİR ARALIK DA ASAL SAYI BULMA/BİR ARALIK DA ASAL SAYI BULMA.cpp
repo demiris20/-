86a68efb31b0adfbba80d9fcbc6837a6907f6a26
İsmail Demir <109111495+demiris20@users.noreply.github.com>
2024-10-06 14:53:15 +0300

    Add files via upload

---
+++ BİR ARALIK DA ASAL SAYI BULMA/BİR ARALIK DA ASAL SAYI BULMA/BİR ARALIK DA ASAL SAYI BULMA.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int a;
+	cout << "Bir sayi giriniz";
+	cin >> a;
+	
+int sayac = 0;
+
+	for (int x = 0; x <= a; x++) {
+		while (true) {
+
+			
+			
+
+			for (int i = 2; i < x; i++) {
+				if (x % i == 0) {
+					
+					sayac++;
+					
+				}
+				else if (x == 1) {
+					
+					sayac++;
+					
+				}
+				else if (x == 2) {
+					cout << x<<"\n";
+					sayac++;
+					
+				}
+			}
+			if (sayac == 0) {
+				cout << x<< "\n";
+				break;
+			}
+		}
+	}
+
+	
+
+
+
+
+
+
+	
+}
+
+
+
